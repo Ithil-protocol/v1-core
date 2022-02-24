9171d59ebdcf28e86787852982555dce40511be0
ChainDev931105 <kaitoyamamoto931105@outlook.com>
2022-02-24 21:32:37 +0000

    build: test Vault.borrow.ts

--- test/vault/Vault.ts
+++ test/vault/Vault.ts
@@ -1,13 +1,14 @@
 import { artifacts, ethers, waffle } from "hardhat";
 import type { Artifact } from "hardhat/types";
 import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
-import { expect } from "chai";
-
 import type { Vault } from "../../src/types/Vault";
 import { Signers } from "../types";
 import { MockKyberNetworkProxy } from "../../src/types/MockKyberNetworkProxy";
 import { MockWETH } from "../../src/types/MockWETH";
-import { BigNumber } from "ethers";
+
+import { checkWhiteList } from "./Vault.whiteList";
+import { checkStake } from "./Vault.stake";
+import { checkBorrow } from "./Vault.borrow";
 
 describe("Unit tests", function () {
   before(async function () {
@@ -37,67 +38,7 @@ describe("Unit tests", function () {
     });
 
     checkWhiteList(); // whitelistToken, whitelistTokenAndExec
-    // checkStake(); // stake, unstake
-    // checkBorrow(); // borrow, repay
+    checkStake(); // stake, unstake
+    // checkBorrow(); // borrow, repay // TODO: currently, skip borrow checking because it is strategyOnly
   });
 });
-
-function checkWhiteList(): void {
-  it("check whitelistToken", async function () {
-    const baseFee = 10;
-    const fixedFee = 11;
-    const token = this.mockWETH.address;
-    const initialState = {
-      vaultState: await this.vault.vaults(token),
-    };
-
-    await this.vault.whitelistToken(token, baseFee, fixedFee);
-
-    const finalState = {
-      vaultState: await this.vault.vaults(token),
-    };
-    console.log(initialState, finalState);
-
-    expect(initialState.vaultState.supported).to.equal(false);
-    expect(finalState.vaultState.supported).to.equal(true);
-    expect(finalState.vaultState.baseFee).to.equal(BigNumber.from(baseFee));
-    expect(finalState.vaultState.fixedFee).to.equal(BigNumber.from(fixedFee));
-  });
-
-  it("check whitelistTokenAndExec", async function () {
-    const baseFee = 10;
-    const fixedFee = 11;
-    const OUSD = "0x2A8e1E676Ec238d8A992307B495b45B3fEAa5e86";
-    let ABI = '[{"inputs": [],"name": "rebaseOptIn","outputs": [],"stateMutability": "nonpayable","type": "function"}]';
-    let iface = new ethers.utils.Interface(ABI);
-    const data = iface.encodeFunctionData("rebaseOptIn");
-
-    const initialState = {
-      vaultState: await this.vault.vaults(OUSD),
-    };
-
-    await this.vault.whitelistTokenAndExec(OUSD, baseFee, fixedFee, data);
-
-    const finalState = {
-      vaultState: await this.vault.vaults(OUSD),
-    };
-
-    expect(initialState.vaultState.supported).to.equal(false);
-    expect(finalState.vaultState.supported).to.equal(true);
-    expect(finalState.vaultState.baseFee).to.equal(BigNumber.from(baseFee));
-    expect(finalState.vaultState.fixedFee).to.equal(BigNumber.from(fixedFee));
-  });
-}
-
-function checkStake(): void {
-  it("check stake", async function () {
-    const amount = ethers.utils.parseUnits("1.0", 18);
-    // await this.vault.connect(this.signers.investor).stake(addresses.MockTaxedToken, amount);
-  });
-  it("check unstake", async function () {});
-}
-
-function checkBorrow(): void {
-  it("check borrow", async function () {});
-  it("check repay", async function () {});
-}
