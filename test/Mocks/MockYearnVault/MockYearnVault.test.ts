e157846f2b2fc70d91a2faee2ef08c4342413963
ChainDev931105 <kaitoyamamoto931105@outlook.com>
2022-03-08 20:04:30 +0000

    build: initial test structure

---
+++ test/Mocks/MockYearnVault/MockYearnVault.test.ts
@@ -0,0 +1,19 @@
+import { artifacts, ethers, waffle } from "hardhat";
+import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
+import { Signers } from "../../types";
+
+describe("Unit tests", function () {
+  before(async function () {
+    this.signers = {} as Signers;
+
+    const signers: SignerWithAddress[] = await ethers.getSigners();
+    this.signers.admin = signers[0];
+    this.signers.investor = signers[1];
+    this.signers.trader = signers[2];
+    this.signers.liquidator = signers[3];
+  });
+
+  describe("MockYearnVault", function () {
+    beforeEach(async function () {});
+  });
+});
---
+++ test/Strategies/MarginTradingStrategy/MarginTradingStrategy.openPosition.ts
@@ -0,0 +1,7 @@
+import { expect } from "chai";
+import { BigNumber } from "ethers";
+import { ethers } from "hardhat";
+
+export function checkAddStrategy(): void {
+  it("Vault: addStrategy", async function () {});
+}
