95a076c57ca36a910adebae3d0d620422f3db6e4
ChainDev931105 <kaitoyamamoto931105@outlook.com>
2022-03-08 20:37:23 +0000

    build: test liquidator

--- test/Liquidator/Liquidator.liquidateSingle.ts
+++ test/Liquidator/Liquidator.liquidateSingle.ts
@@ -2,6 +2,6 @@ import { expect } from "chai";
 import { BigNumber } from "ethers";
 import { ethers } from "hardhat";
 
-export function checkAddStrategy(): void {
-  it("Vault: addStrategy", async function () {});
+export function checkLiquidateSingle(): void {
+  it("Liquidator: liquidateSingle", async function () {});
 }
--- test/Liquidator/Liquidator.test.ts
+++ test/Liquidator/Liquidator.test.ts
@@ -1,6 +1,11 @@
 import { artifacts, ethers, waffle } from "hardhat";
+import { Artifact } from "hardhat/types";
 import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
 import { Signers } from "../types";
+import { Liquidator } from "../../src/types/Liquidator";
+import { checkLiquidateSingle } from "./Liquidator.liquidateSingle";
+import { checkMarginCall } from "./Liquidator.marginCall";
+import { checkPurchaseAssets } from "./Liquidator.purchaseAssets";
 
 describe("Unit tests", function () {
   before(async function () {
@@ -14,6 +19,13 @@ describe("Unit tests", function () {
   });
 
   describe("Liquidator", function () {
-    beforeEach(async function () {});
+    beforeEach(async function () {
+      const liquidatorArtifact: Artifact = await artifacts.readArtifact("Liquidator");
+      this.liquidator = <Liquidator>await waffle.deployContract(this.signers.admin, liquidatorArtifact, []);
+    });
+
+    checkLiquidateSingle();
+    checkMarginCall();
+    checkPurchaseAssets();
   });
 });
