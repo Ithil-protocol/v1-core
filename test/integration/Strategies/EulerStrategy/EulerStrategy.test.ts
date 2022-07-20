f640921382214e874d92c0ecf5a08e5a5040256a
lsqrl <lsqrl91@gmail.com>
2022-07-20 11:27:09 +0200

    feat: fixture for Euler

--- test/integration/Strategies/EulerStrategy/EulerStrategy.test.ts
+++ test/integration/Strategies/EulerStrategy/EulerStrategy.test.ts
@@ -1,4 +1,5 @@
 import { artifacts, ethers, waffle } from "hardhat";
+import { BigNumber, Wallet } from "ethers";
 import type { Artifact } from "hardhat/types";
 import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
 import type { Vault } from "../../../../src/types/Vault";
@@ -6,58 +7,95 @@ import { Signers } from "../../../types";
 import type { ERC20 } from "../../../../src/types/ERC20";
 
 import { tokens } from "../../../common/mainnet";
-import { euler, eulerMarkets } from "./constants";
-import { getTokens } from "../../../common/utils";
-import { marginTokenLiquidity } from "../../../common/params";
+import { euler, eulerMarkets, etoken } from "./constants";
+import { marginTokenLiquidity, marginTokenMargin, leverage } from "../../../common/params";
+import { getTokens, expandToNDecimals, fundVault } from "../../../common/utils";
 
 import { EulerStrategy } from "../../../../src/types/EulerStrategy";
 import { Liquidator } from "../../../../src/types/Liquidator";
 
-import { checkPerformInvestment } from "./EulerStrategy.invest";
+import { eulerFixture } from "./fixture";
+
+const deadline = Math.floor(Date.now() / 1000) + 60 * 20; // 20 minutes from the current Unix time
+
+const createFixtureLoader = waffle.createFixtureLoader;
+
+type ThenArg<T> = T extends PromiseLike<infer U> ? U : T;
+
+let wallet: Wallet, other: Wallet;
+
+let WETH: ERC20;
+let admin: SignerWithAddress;
+let trader1: SignerWithAddress;
+let trader2: SignerWithAddress;
+let liquidator: SignerWithAddress;
+let createStrategy: ThenArg<ReturnType<typeof eulerFixture>>["createStrategy"];
+let loadFixture: ReturnType<typeof createFixtureLoader>;
+
+let vault: Vault;
+let liquidatorContract: Liquidator;
+let strategy: EulerStrategy;
+let tokensAmount: BigNumber;
+
+let marginToken: ERC20;
+let investmentToken: ERC20;
+
+let order: {
+  spentToken: string;
+  obtainedToken: string;
+  collateral: BigNumber;
+  collateralIsSpentToken: boolean;
+  minObtained: BigNumber;
+  maxSpent: BigNumber;
+  deadline: number;
+};
 
 describe("Euler strategy integration tests", function () {
-  before(async function () {
-    this.signers = {} as Signers;
+  before("create fixture loader", async () => {
+    [wallet, other] = await (ethers as any).getSigners();
+    loadFixture = createFixtureLoader([wallet, other]);
+  });
 
+  before("load fixtures", async () => {
+    ({ WETH, admin, trader1, trader2, liquidator, vault, liquidatorContract, createStrategy } = await loadFixture(
+      eulerFixture,
+    ));
+    strategy = await createStrategy();
+  });
+
+  before("prepare vault with default parameters", async () => {
     const signers: SignerWithAddress[] = await ethers.getSigners();
-    this.signers.admin = signers[0];
-    this.signers.investor = signers[1];
-    this.signers.trader = signers[2];
-    this.signers.liquidator = signers[3];
+    const staker = signers[1];
+
+    const tokenArtifact: Artifact = await artifacts.readArtifact("ERC20");
+    marginToken = <ERC20>await ethers.getContractAt(tokenArtifact.abi, tokens.DAI.address);
+    investmentToken = <ERC20>await ethers.getContractAt(tokenArtifact.abi, tokens.WETH.address);
+
+    await vault.whitelistToken(marginToken.address, 10, 10, 1000);
+    await vault.whitelistToken(investmentToken.address, 10, 10, 1);
+
+    await getTokens(staker.address, marginToken.address, tokens.DAI.whale, marginTokenLiquidity);
+    await getTokens(trader1.address, marginToken.address, tokens.DAI.whale, marginTokenLiquidity);
+    await fundVault(signers[1], vault, marginToken, marginTokenLiquidity);
+
+    await marginToken.connect(trader1).approve(strategy.address, marginTokenMargin);
+
+    order = {
+      spentToken: marginToken.address,
+      obtainedToken: etoken,
+      collateral: marginTokenMargin,
+      collateralIsSpentToken: true,
+      minObtained: marginTokenMargin, // todo: refine
+      maxSpent: marginTokenMargin.mul(leverage),
+      deadline: deadline,
+    };
   });
 
-  describe("EulerStrategy", function () {
-    beforeEach(async function () {
-      const tokenArtifact: Artifact = await artifacts.readArtifact("ERC20");
-      this.weth = <ERC20>await ethers.getContractAt(tokenArtifact.abi, tokens.WETH.address);
-
-      this.dai = <ERC20>await ethers.getContractAt(tokenArtifact.abi, tokens.DAI.address);
-      await getTokens(this.signers.investor.address, tokens.DAI.address, tokens.DAI.whale, marginTokenLiquidity);
-      await getTokens(this.signers.trader.address, tokens.DAI.address, tokens.DAI.whale, marginTokenLiquidity);
-
-      const vaultArtifact: Artifact = await artifacts.readArtifact("Vault");
-      this.vault = <Vault>await waffle.deployContract(this.signers.admin, vaultArtifact, [this.weth.address]);
-
-      const liquidatorArtifact: Artifact = await artifacts.readArtifact("Liquidator");
-      this.liquidator = <Liquidator>(
-        await waffle.deployContract(this.signers.admin, liquidatorArtifact, [
-          "0x0000000000000000000000000000000000000000",
-        ])
-      );
-
-      const esArtifact: Artifact = await artifacts.readArtifact("EulerStrategy");
-      this.eulerStrategy = <EulerStrategy>(
-        await waffle.deployContract(this.signers.admin, esArtifact, [
-          this.vault.address,
-          this.liquidator.address,
-          eulerMarkets,
-          euler,
-        ])
-      );
-
-      await this.vault.addStrategy(this.eulerStrategy.address);
-    });
-
-    checkPerformInvestment();
+  it("Euler Strategy: open position", async function () {
+    await strategy.connect(trader1).openPosition(order);
   });
+
+  // await this.eulerStrategy
+  // .connect(trader)
+  // .closePosition(1, maxSpent, { gasPrice: ethers.utils.parseUnits("500", "gwei"), gasLimit: 30000000 });
 });
