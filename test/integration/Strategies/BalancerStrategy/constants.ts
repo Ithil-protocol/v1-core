b03f9a5e4a6d8feda468b95957310eaa10d3eb1a
OxMarco <giammarco.casanova@gmail.com>
2022-09-30 22:35:44 +0200

    DAI-WETH pool

--- test/integration/Strategies/BalancerStrategy/constants.ts
+++ test/integration/Strategies/BalancerStrategy/constants.ts
@@ -1,3 +1,3 @@
 export const balancerVault = "0xBA12222222228d8Ba445958a75a0704d566BF2C8";
-export const balancerPoolAddress = "0x06df3b2bbb68adc8b0e302443692037ed9f91b42";
-export const balancerPoolID = "0x06df3b2bbb68adc8b0e302443692037ed9f91b42000000000000000000000063";
+export const balancerPoolAddress = "0x0b09dea16768f0799065c475be02919503cb2a35";
+export const balancerPoolID = "0x0b09dea16768f0799065c475be02919503cb2a3500020000000000000000001a";
